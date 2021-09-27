471b43d52b8d1d76b6916725cad302fb527bc6af
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2021-09-27 15:49:53 -0300

    fetch api

--- treinamento-web/olaMundo/script.js
+++ treinamento-web/olaMundo/script.js
@@ -8,17 +8,21 @@ function linguagem() {
   let boleano = true;
   let texto = '';
   let data = new Date(); //data de hoje
+
   typeof numero;
   typeof boleano;
   typeof texto;
   typeof data;
+
   numero == '0'; //true
   numero === 0; //true
   !boleano; //false
   data > new Date(2020, 01, 01); //true
+
   // null e undefined
   let indefinido;
   let nulo = null;
+
   typeof indefinido; //undefined
   typeof nulo; // object
 
@@ -30,26 +34,30 @@ function linguagem() {
   for (let i = 0; i < lista.length; i++) {
     console.log(lista[i]);
   }
-  // foreach
+
   for (const valor of lista) {
     console.log(valor);
   }
-  // while
+
   while (false) { }
-  // do-while
+
   do { } while (false);
-  // forEach do obj array
-  lista.forEach(() => { });
+
+  lista.forEach((valor) => {
+    console.log(valor);
+  });
 
   // unica linha
   if (lista) console.log('array');
   else console.log('objeto');
+
   // bloco
   if (objeto) {
     console.log('objeto');
   } else {
     console.log('vazio');
   }
+
   // switch case
   let x = 1
   switch (x) {
@@ -59,6 +67,7 @@ function linguagem() {
     default:
       console.log('desconhecido');
   }
+
   //ternário
   true ? 'verdadeiro' : 'falso';
 }
@@ -66,34 +75,106 @@ function linguagem() {
 const funcao = function () {
   linguagem();
 }
+
 const funcao2 = () => {
   funcao();
 }
 
 /**
- * Executa o submit do formulário da página
- * @param {Event} event Evento de submit
- */
+* Variável que armezena o novo indice
+*/
+let novoId = 0;
+
+/**
+* Executa o submit do formulário da página
+* @param {Event} event Evento de submit
+*/
 const onSubmit = (event) => {
   event.preventDefault();
-  const submitedValue = {
-    nome: event.target.nome.value,
-    tipo: event.target.tipo.value,
-  };
-  console.log(submitedValue);
+  const ulEl = document.querySelector('.main ul');
+
+  if (ulEl) {
+    const submitedValue = {
+      nome: event.target.nome.value,
+      tipo: event.target.tipo.value,
+    };
+
+    // Trabalhando com promises sem uso do async/await
+    putData(novoId, submitedValue).then(novaSubmissao => {
+      novoId++;
+      addListItem(novaSubmissao, ulEl);
+    });
+  }
 };
+
 /**
- * executa após carregamento do conteúdo do DOM
- */
+* Consulta dados na API
+* @returns {Promise<{ tipo: number; nome: string }[]>} Lista de submissões
+*/
+const getData = () =>
+  fetch('')
+    .then(response => response.json())
+    .catch(err => console.error(err));
+
+/**
+* Envia dados para API
+* @param {number} novoIndice
+* @param {{ tipo: number; nome: string }} novaSubmissao
+* @returns {Promise<{ tipo: number; nome: string }>} Nova submissão
+*/
+const putData = (novoIndice, novaSubmissao) =>
+  fetch(`${novoIndice}.json`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(novaSubmissao)
+  })
+    .then(response => response.json())
+    .catch(err => console.error(err));
+
+/**
+* Adiciona item na lista do html
+* @param {{ tipo: number; nome: string }} item
+* @param {HTMLUListElement} ulEl 
+*/
+const addListItem = (item, ulEl) => {
+  const liEl = document.createElement("li");
+  liEl.innerText = `${item.nome} - Tipo: ${item.tipo}`;
+  ulEl.appendChild(liEl);
+}
+
+/**
+* Adiciona lista de submits consultada
+* @param {HTMLUListElement} ulEl 
+*/
+const addSubmits = async (ulEl) => {
+  // Trabalhando com promises usando async/await
+  const result = await getData();
+
+  // Se result existir e tiver elementos no array
+  if (result && result.length) {
+    novoId = result.length;
+    result.forEach((item) => addListItem(item, ulEl));
+  }
+}
+
+/**
+* executa após carregamento do conteúdo do DOM
+*/
 const onLoad = () => {
-  const footerEl = document.querySelector('.footer p');
-  const formEl = document.querySelector('form');
+  const footerEl = document.querySelector(".footer p");
+  const formEl = document.querySelector("form");
+  const ulEl = document.querySelector(".submits ul");
   // verifica o elemento buscado
   if (footerEl)
     footerEl.innerHTML += ` - ${new Date().getFullYear()}`;
   // verifica o elemento buscado
   if (formEl)
     formEl.addEventListener('submit', onSubmit);
+  // verifica o elemento buscado
+  if (ulEl)
+    addSubmits(ulEl);
   // remove evento do documento
   document.removeEventListener('DOMContentLoaded', onLoad);
 };
