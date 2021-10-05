4a1c73a888c9d725b611fd80180ff6e99e864460
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2021-10-05 09:36:31 -0300

    ADD: lógica para acertos

--- treinamento-react/jogodaforca/src/components/Forca/Forca.jsx
+++ treinamento-react/jogodaforca/src/components/Forca/Forca.jsx
@@ -8,7 +8,23 @@ function Forca() {
   const [caractere, setCarcatere] = useState()
 
   function verificaCaractere() {
-
+    var i = 0;
+    var posicao = 0;
+    var liEl = null;
+    if (caractere !== "") {
+      if (word.indexOf(caractere) !== -1) {
+        while (i < word.length) {
+          posicao = word.indexOf(caractere, i)
+          if (posicao !== -1) {
+            liEl = document.getElementById(posicao);
+            liEl.innerHTML = caractere;
+          }
+          i++;
+        }
+      }
+    } else {
+      alert("INSIRA UMA LETRA PARA JOGAR!");
+    }
   }
 
   function inputChangeCallBack(event) {
@@ -20,7 +36,7 @@ function Forca() {
       <div className="hidden-word">
         <ul>
           {word.map((caractere, index) => {
-            return <li key={index}>_</li>;
+            return <li key={index} id={index}>_</li>;
           })}
         </ul>
       </div>
