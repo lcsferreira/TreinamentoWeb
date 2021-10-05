4fbe5c9f542367996ad4b0fe3ec5009875eb6d41
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2021-10-05 10:31:01 -0300

    ADD: lógica fim de jogo

--- treinamento-react/jogodaforca/src/components/Forca/Forca.jsx
+++ treinamento-react/jogodaforca/src/components/Forca/Forca.jsx
@@ -5,14 +5,17 @@ import usePalavraSecreta from '../../services/usePalavraSecreta';
 function Forca() {
   const { palavra } = usePalavraSecreta();
   const word = palavra.split("");
-  const [caractere, setCaractere] = useState()
-  var [erros, setErros] = useState(0);
+  const [caractere, setCaractere] = useState("")
+  var [erros, setErros] = useState(6);
 
   function verificaCaractere() {
     var i = 0;
     var posicao = 0;
     var liEl = null;
-    if (caractere !== "") {
+    if (caractere === "") {
+      alert("INSIRA UMA LETRA PARA JOGAR!");
+      return;
+    } else {
       if (word.indexOf(caractere) !== -1) {
         while (i < word.length) {
           posicao = word.indexOf(caractere, i)
@@ -22,31 +25,69 @@ function Forca() {
           }
           i++;
         }
+        document.getElementById('letra').value = "";
+        setCaractere("");
+        setTimeout(() => {
+          verificaFimDeJogo();
+        }, 100)
       } else {
-        erros += 1;
+        erros -= 1;
         setErros(erros);
-        if (erros === 1) {
+        if (erros === 5) {
           const imgEl = document.querySelector('#cabeca');
           imgEl.style.display = "block"
-        } else if (erros === 2) {
+        } else if (erros === 4) {
           const imgEl = document.querySelector('#corpo');
           imgEl.style.display = "block"
         } else if (erros === 3) {
           const imgEl = document.querySelector('#bracoEsq');
           imgEl.style.display = "block"
-        } else if (erros === 4) {
+        } else if (erros === 2) {
           const imgEl = document.querySelector('#bracoDir');
           imgEl.style.display = "block"
-        } else if (erros === 5) {
+        } else if (erros === 1) {
           const imgEl = document.querySelector('#pernaEsq');
           imgEl.style.display = "block"
-        } else if (erros === 6) {
+        } else if (erros === 0) {
           const imgEl = document.querySelector('#pernaDir');
           imgEl.style.display = "block"
         }
+        document.getElementById('letra').value = "";
+        setCaractere("");
+        setTimeout(() => {
+          verificaFimDeJogo();
+        }, 100)
       }
+    }
+  }
+
+  function verificaFimDeJogo() {
+    var lis = document.querySelectorAll('li');
+    var j = 0;
+    var liEl = null;
+
+    if (erros === 0) {
+      while (j < word.length) {
+        liEl = document.getElementById(j);
+        liEl.innerHTML = word[j];
+        j++;
+      }
+      lis.forEach(li => { li.style.color = "red" })
+      setTimeout(() => {
+        alert("DEU FORCA!!");
+        window.location.reload(true);
+      }, 100)
     } else {
-      alert("INSIRA UMA LETRA PARA JOGAR!");
+      for (var i = 0; i < lis.length; i++) {
+        if (lis[i].innerText === '_') {
+          return;
+        }
+      }
+      lis.forEach(li => { li.style.color = "green" })
+      setTimeout(() => {
+        alert("PARABÉNS, VOCÊ SALVOU O STEVE!");
+        window.location.reload(true);
+      }, 100)
     }
   }
 
@@ -79,7 +120,7 @@ function Forca() {
         </ul>
       </div>
       <div className="game-inputs">
-        <input type="text" onChange={inputChangeCallBack} />
+        <input type="text" valeu="" id="letra" onChange={inputChangeCallBack} />
         <button type="submit" onClick={verificaCaractere}>JOGAR</button>
       </div>
     </div>
